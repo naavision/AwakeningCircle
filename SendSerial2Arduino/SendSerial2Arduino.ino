840930e815c9492be81f848d1c68b56f43548241
Danijel Sivinjski <danijel@sivinjski.com>
2015-07-15 12:27:47 +0200

    ~

--- SendSerial2Arduino/SendSerial2Arduino.ino
+++ SendSerial2Arduino/SendSerial2Arduino.ino
@@ -1,16 +1,18 @@
 // Understanding values from vvvv [RS232 (Devices String Legacy) node] and
 // sending them to the array of 360 APA102 pixels (serial), 6 strips x 60 pixels
 
-// Arduino Mega 2560
-// DATA pin A9 (analog)
-// CLOCK pin A10 (analog) 
+// Arduino Mega 2560, DATA pin A9 (analog), CLOCK pin A10 (analog)
+// *analogWrite() works on pins 2 - 13 and 44 - 46.
+
+int dataPin = A9; // DATA Pin
+int clockPin = A10;  // CLOCK Pin
 
 char packetBuffer[6];
+
 void setup()
 {
-  // Output pins
-  pinMode(9, OUTPUT); // DATA
-  pinMode(10, OUTPUT); // CLOCK
+  pinMode(dataPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -19,17 +21,15 @@ void loop() {
 if (Serial.available() > 0) 
 {
  Serial.readBytes(packetBuffer,8); // 6 is the packetsize
- if( packetBuffer[0]=='<' && packetBuffer[6-1]=='>')
+ if( packetBuffer[0]=='<' && packetBuffer[6-1]== '>')
   {
   unsigned char CleanBuffer [6-2];
   memcpy(CleanBuffer,packetBuffer+1,6-2);
   // Now we have a clean input buffer of 4 bytes to use.
    
-  analogWrite(9,CleanBuffer[0]); 
-  analogWrite(10,CleanBuffer[1]); 
-  analogWrite(11,CleanBuffer[2]); 
-  analogWrite(12,CleanBuffer[3]); // (test) changed from digitalWrite to analogWrite
+  analogWrite(dataPin,CleanBuffer[0]); 
+  analogWrite(clockPin,CleanBuffer[1]); 
   }
 }
-delay(5);
+delay(4); // wait 4 milliseconds before the next loop
 }
